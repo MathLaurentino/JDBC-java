6b96e9ff347c91bd036bd8cf91594e4c9b7665f8
Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-03-09 15:30:02 -0300

    SellerDaoJDBC, DaoFactory and findById implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -11,7 +11,7 @@ public class Seller implements Serializable {
     private Integer id;
     private String name;
     private String email;
-    private Date birthday;
+    private Date birthDate;
     private Double baseSalary;
 
     private Department department;
@@ -20,11 +20,11 @@ public class Seller implements Serializable {
 
     }
 
-    public Seller(Integer id, String name, String email, Date birthday, Double baseSalary, Department department) {
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.birthday = birthday;
+        this.birthDate = birthDate;
         this.baseSalary = baseSalary;
         this.department = department;
     }
@@ -53,12 +53,12 @@ public class Seller implements Serializable {
         this.email = email;
     }
 
-    public Date getBirthday() {
-        return birthday;
+    public Date getBirthDate() {
+        return birthDate;
     }
 
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
     }
 
     public Double getBaseSalary() {
@@ -96,7 +96,7 @@ public class Seller implements Serializable {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
-                ", birthday=" + birthday +
+                ", birthday=" + birthDate +
                 ", baseSalary=" + baseSalary +
                 ", department=" + department +
                 '}';
