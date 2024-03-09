2002fc0fd0915cce70c6eadb6690656453c51f7e
Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-03-09 00:10:32 -0300

    Add Seller and Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
-import db.DB;
+import model.entities.Department;
+import model.entities.Seller;
 
-import java.sql.Connection;
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+
+        Department obj = new Department(1, "Books");
+//        System.out.println(obj);
+        Seller seller = new Seller(21, "matheus", "math@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
+
 
 }
